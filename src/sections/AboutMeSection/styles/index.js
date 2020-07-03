f4b56db181c75c997b796a37aaaa8f1d8a866297
Xavier Rincon Rico <xavirr1994@gmail.com>
2020-07-03 17:41:04 +0200

    Remove unnecesary styles

--- src/sections/AboutMeSection/styles/index.js
+++ src/sections/AboutMeSection/styles/index.js
@@ -1,13 +1,5 @@
 export const styles = theme => {
   return {
-    section: {
-      width: '100%',
-      height: '100vh',
-      paddingTop: 40,
-      letterSpacing: 6
-    },
-    firstPageTitle: {
-      textAlign: 'center'
-    }
+
   }
 }
--- src/sections/TrajectorySection/styles/index.js
+++ src/sections/TrajectorySection/styles/index.js
@@ -1,11 +1,5 @@
 export const styles = theme => {
   return {
-    section: {
-    },
-    title: {
-      textAlign: 'center',
-      marginBottom: 50
-    },
     experience: {
 
     }
