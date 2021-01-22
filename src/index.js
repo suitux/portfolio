dc02b2730bc9e58e3c6487ff8aeb324915071d04
Codex408194 <xavirr1994@gmail.com>
2021-01-22 16:20:44 +0100

    Added Google Analytics

--- src/index.js
+++ src/index.js
@@ -5,7 +5,19 @@ import store from './redux/store'
 import { MuiThemeProvider } from '@material-ui/core'
 import theme from './theme'
 import CssBaseline from '@material-ui/core/CssBaseline'
-import { PortfolioPage } from "./pages/Portfolio";
+import { PortfolioPage } from './pages/Portfolio'
+import firebase from 'firebase'
+
+firebase.initializeApp({
+  apiKey: 'AIzaSyBT5UWSOraUWmb5cQZS6e53wckm18iYduw',
+  authDomain: 'portfolio-1539542541675.firebaseapp.com',
+  databaseURL: 'https://portfolio-1539542541675.firebaseio.com',
+  projectId: 'portfolio-1539542541675',
+  storageBucket: 'portfolio-1539542541675.appspot.com',
+  messagingSenderId: '231004710803',
+  appId: '1:231004710803:web:c7543653ff0af673886a66'
+})
+firebase.analytics()
 
 ReactDOM.render(
   <MuiThemeProvider theme={theme}>
