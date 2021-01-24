5555e60bdfac32fc0a009d67431b4428af1e6680
Codex408194 <xavirr1994@gmail.com>
2021-01-24 18:49:01 +0100

    Updated skills

--- src/sections/SkillsSection/data/index.js
+++ src/sections/SkillsSection/data/index.js
@@ -1,47 +1,47 @@
 export const skills = [
   {
     title: 'React JS',
-    value: '80',
+    value: 85,
     image: 'https://www.freecodecamp.org/news/content/images/size/w2000/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png'
   },
   {
     title: 'Clean Code',
-    value: '85',
+    value: 85,
     image: 'https://nonstoplearningbydoing.files.wordpress.com/2018/12/cleancode.jpg?w=640'
   },
   {
     title: 'Java',
-    value: '75',
+    value: 75,
     image: 'https://www.oracle.com/a/ocom/img/cb71-java-logo.png'
   },
   {
     title: 'Selenium',
-    value: '95',
+    value: 90,
     image: 'https://cdn.mindmajix.com/courses/selenium-training.png'
   },
   {
     title: '.NET',
-    value: '85',
+    value: 85,
     image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/C_Sharp_logo.svg/1200px-C_Sharp_logo.svg.png'
   },
   {
     title: 'Node JS',
-    value: '40',
+    value: 65,
     image: './src/img/skills/node.png'
   },
   {
     title: 'SQL',
-    value: '80',
+    value: 80,
     image: 'https://s3-us-west-2.amazonaws.com/devcodepro/media/blog/que-es-sql.png'
   },
   {
     title: 'Jenkins',
-    value: '90',
+    value: 90,
     image: 'https://dwglogo.com/wp-content/uploads/2017/11/1500px_Jenkins_logo.png'
   },
   {
     title: 'PHP',
-    value: '65',
+    value: 75,
     image: './src/img/skills/php.png'
   }
 ]
